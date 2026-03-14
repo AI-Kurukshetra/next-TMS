e814af44c6f29bd62226751af66b9f81ed2ac7cf
jenish2025 <jenish.patel@bacancy.com>
2026-03-14 20:08:32 +0530

    playwrite video issue resolved

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,10 +24,6 @@ export default defineConfig({
       use: {
         ...devices["Desktop Chrome"],
         viewport: { width: 1920, height: 1080 },
-        video: {
-          dir: "demo-videos",
-          size: { width: 1920, height: 1080 },
-        },
       },
     },
   ],
